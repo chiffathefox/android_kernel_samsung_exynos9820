3f59ac3d0c8017af0e53ba29e3e4267b7f971d75
chiffathefox <chiffathefoxx@gmail.com>
2026-01-19 19:33:37 +0200

    add a log

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -7905,6 +7905,7 @@ static int ufshcd_probe_hba(struct ufs_hba *hba)
 	ktime_t start = ktime_get();
 	unsigned long flags;
 
+	dev_err(hba->dev, "%s\n", __func__);
 retry:
 	ret = ufshcd_hba_enable(hba);
 	if (ret)
