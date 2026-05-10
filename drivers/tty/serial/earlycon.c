7516f9372b4b50e86d2105de061e37f780ac6c52
chiffathefox <chiffathefoxx@gmail.com>
2026-05-10 19:50:04 +0300

    back to 4210

--- drivers/tty/serial/earlycon.c
+++ drivers/tty/serial/earlycon.c
@@ -227,9 +227,10 @@ static int __init param_setup_earlycon(char *buf)
 		}
 	}
 
-	err = setup_earlycon("exynos,mmio32,0x10440000");
+	err = setup_earlycon("exynos4210,mmio32,0x10440000");
 	if (err == -ENOENT || err == -EALREADY)
 		return 0;
+	printk("hello from earlycon!");
 	return err;
 }
 early_param("earlycon", param_setup_earlycon);
